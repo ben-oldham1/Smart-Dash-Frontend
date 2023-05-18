12c7d96d5849180b59a41d0191023ebc59fa8083
Ben <ben.oldham2003@gmail.com>
2023-05-18 14:56:47 +0100

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import Tile from './components/Tile';
+import Environment from './components/Environment';
+import Today from './components/Today';
+import News from './components/News';
+import Security from './components/Security';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+
+      <Row className='my-3'>
+
+        {/* === Left column === */} 
+        <Col xs={6}>
+
+          <Row>
+            <Tile title={'TODAY'}>
+              <Today />
+            </Tile>
+          </Row>
+
+          <Row>
+            <Tile title={'ENVIRONMENT'}>
+              <Environment />
+            </Tile>
+          </Row>
+
+          <Row>
+            <Tile title={'SECURITY'}>
+              <Security />
+            </Tile>
+          </Row>
+
+        </Col>
+
+
+        {/* === Right column === */} 
+        <Col xs={6}>
+          <Row>
+            <Tile title={'NEWS'}>
+              <News />
+            </Tile>
+          </Row>
+
+          <Row>
+            <Tile title={'TRANSPORT'}>
+              <Environment />
+            </Tile>
+          </Row>
+        </Col>
+
+      </Row>
+
+    </Container>
   );
 }
 
