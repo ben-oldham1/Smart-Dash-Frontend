e52ca4598cd9999d952d122e2ec9f6f8a6b8fed5
Ben <ben.oldham2003@gmail.com>
2023-05-21 12:55:22 +0100

    Data is now live

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,29 +1,96 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 export default function Today(props) {
+
+    const [currentDateTime, setCurrentDateTime] = useState(new Date());
+    const [sunrise, setSunrise] = useState(null);
+    const [sunset, setSunset] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentDateTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    useEffect(() => {
+        const fetchSunriseSunset = () => {
+            const apiKey = 'cadbd0ffe610d790aa93e83718ef070d';
+            const url = `https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid=${apiKey}`;
+
+            // Get user's current location
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const { latitude, longitude } = position.coords;
+
+                    // Fetch weather data to get sunrise and sunset times
+                    fetch(url.replace('{latitude}', latitude).replace('{longitude}', longitude))
+                        .then((response) => response.json())
+                        .then((data) => {
+                            setSunrise(new Date(data.sys.sunrise * 1000).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' }));
+                            setSunset(new Date(data.sys.sunset * 1000).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' }));
+                            setLoading(false);
+
+                            // Calculate time remaining until next midnight
+                            const now = new Date();
+                            const nextMidnight = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
+                            const timeRemaining = nextMidnight - now;
+
+                            // Fetch sunrise and sunset times after time remaining
+                            setTimeout(fetchSunriseSunset, timeRemaining);
+                        })
+                        .catch((error) => {
+                            console.error('Error fetching sunrise and sunset times:', error);
+                            setLoading(false);
+                        });
+                },
+                (error) => {
+                    console.error('Error getting current location:', error);
+                    setLoading(false);
+                }
+            );
+        };
+
+        fetchSunriseSunset(); // Fetch initially
+
+    }, []);
+
+    const formatDate = (date) => {
+        const options = { day: 'numeric', month: 'short' };
+        const parts = date.toLocaleDateString('en-US', options).split(' ');
+
+        // Rearranges the date. E.g. 'Jan 10' becomes '10 Jan'
+        return `${parts[1]} ${parts[0]}`;
+    };
+
+
     return (
         <>
             <Row className='mb-3'>
-                <Col xs={6}>
+                <Col xs={5}>
                     <h2>
                         DATE
                     </h2>
                     <div className='digit-block'>
-                        <h3>18 May</h3>
+                        <h3>{formatDate(currentDateTime)}</h3>
                     </div>
 
                 </Col>
 
-                <Col xs={3}>
+                <Col xs={4}>
                     <h2>
                         DAY
                     </h2>
                     <div className='digit-block'>
                         <h3>
-                            THU
+                            {currentDateTime.toLocaleDateString('en-US', { weekday: 'long' })}
                         </h3>
                     </div>
                 </Col>
@@ -34,7 +101,7 @@ export default function Today(props) {
                     </h2>
                     <div className='digit-block'>
                         <h3>
-                            2023
+                            {currentDateTime.getFullYear()}
                         </h3>
                     </div>
                 </Col>
@@ -46,7 +113,7 @@ export default function Today(props) {
                         TIME
                     </h2>
                     <div className='digit-block'>
-                        <h3>13:03</h3>
+                        <h3>{currentDateTime.toLocaleTimeString()}</h3>
                     </div>
 
                 </Col>
@@ -57,7 +124,7 @@ export default function Today(props) {
                     </h2>
                     <div className='digit-block'>
                         <h3>
-                            07:52
+                            {loading ? 'Loading...' : sunrise}
                         </h3>
                     </div>
                 </Col>
@@ -68,7 +135,7 @@ export default function Today(props) {
                     </h2>
                     <div className='digit-block'>
                         <h3>
-                            21:07
+                            {loading ? 'Loading...' : sunset}
                         </h3>
                     </div>
                 </Col>
