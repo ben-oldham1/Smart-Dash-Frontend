5ff30eccbd8fddbdbaa38b0096e88ce7650b44a9
Ben <ben.oldham2003@gmail.com>
2023-06-24 13:16:44 +0100

    Added API call to make data live

--- src/components/Environment.js
+++ src/components/Environment.js
@@ -1,69 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
 import TempGauge from './TempGauge'
 
-export default function Environment(props) {
-    return (
-        <>
-            <Card bg={'secondary'} text={'light'} className={'mb-3'}>
-                <Card.Body>
-                    <Card.Title>Weather</Card.Title>
-                    <Card.Text>
-                        <Row>
-                            <Col xs={6}>
-                                <TempGauge temp={20} id={'outsideTemp'} />
-                            </Col>
 
-                            <Col xs={6}>
-                                
-                                <Row>
-                                    <Col className='p-0' xs={6}>
-                                        <p className='text-muted my-0'>
-                                            Feels like
-                                        </p>
-                                        <h4>
-                                            15.8°C
-                                        </h4>
+const Environment = (props) => {
+  const [weatherData, setWeatherData] = useState(null);
+  const [lastUpdated, setLastUpdated] = useState(null);
 
-                                        <p className='text-muted my-0'>
-                                            Humidity
-                                        </p>
-                                        <h4>
-                                            34%
-                                        </h4>
-                                    </Col>
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const apiKey = props.settingsData['weatherApiKey']; // OpenWeatherMap API key
+      const city = props.settingsData['city'] + ',' + props.settingsData['country'];
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-                                    <Col className='p-0' xs={6}>
-                                        <p className='text-muted my-0'>
-                                            Wind
-                                        </p>
-                                        <h4>
-                                            10.4 <small>mph</small>
-                                        </h4>
-                                    </Col>
-                                </Row>
+      try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        setWeatherData(data);
+        setLastUpdated(new Date());
+      } catch (error) {
+        console.log('Error fetching weather data:', error);
+      }
+    };
 
-                            </Col>
-                        </Row>
+    fetchWeatherData();
 
-                        <Row>
-                            <Col xs={12}>
-                                <p className='text-muted my-0'>
-                                    Weather
-                                </p>
-                                <h4>
-                                    Scattered Clouds
-                                </h4>
-                            </Col>
-                        </Row>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </>
-    )
-}
+    const interval = setInterval(fetchWeatherData, 15 * 60 * 1000); // Fetch data every 20 minutes
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  if (!weatherData) {
+    return <p>Loading weather data...</p>;
+  }
+
+  const { main, weather, wind } = weatherData;
+
+  return (
+    <Card bg={'secondary'} text={'light'} className={'mb-3'}>
+      <Card.Body>
+        <Card.Title>WEATHER</Card.Title>
+        <Card.Text>
+          <Row className='mb-2'>
+            <Col xs={5}>
+              <TempGauge temp={main.temp} id={'outsideTemp'} />
+            </Col>
+
+            <Col xs={7}>
+              <Row>
+                <Col xs={6}>
+                  <p className='text-muted my-0'>Feels like</p>
+                  <h4>{main.feels_like}°C</h4>
+
+                  <p className='text-muted my-0'>Humidity</p>
+                  <h4>{main.humidity}%</h4>
+                </Col>
+
+                <Col xs={6}>
+                  <p className='text-muted my-0'>Wind</p>
+                  <h4>
+                    {wind.speed} <small>m/s</small>
+                  </h4>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+
+          <Row className='mb-2'>
+            <Col xs={6}>
+              <p className='text-muted my-0'>Weather</p>
+              <h4>{weather[0].description}</h4>
+            </Col>
+            <Col xs={6}>
+              <p className='text-muted my-0'>Temp today</p>
+              <h4>placeholder...</h4>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs={6}>
+                <Button variant='dark' onClick={() => props.openModal('weatherMap')}>
+                    <i class="bi bi-globe-americas"></i> View map
+                </Button>
+            </Col>
+            <Col xs={6}>
+                <p>Last Updated at: <span className='text-primary'>{lastUpdated.toLocaleTimeString()}</span></p>
+            </Col>
+          </Row>
+
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Environment;
