a9a0591494005b81fc58c5e68f57bca7046c2ee2
Ben <ben.oldham2003@gmail.com>
2023-07-22 13:23:23 +0100

    Add line graph and auto-reload

--- src/components/Speedtest.js
+++ src/components/Speedtest.js
@@ -1,34 +1,56 @@
 import React, { useState, useEffect } from 'react';
-
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import LineGraph from './graphs/internetChart';
 
 const SpeedDisplay = () => {
     const [speedData, setSpeedData] = useState(null);
-
+    const [speedRecords, setSpeedRecords] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
+    const [lastUpdated, setLastUpdated] = useState(null);
+    
     const fetchSpeedData = async () => {
-        const response = await fetch('/speedtest');
-        const data = await response.json();
-        setSpeedData(data);
+        console.log("fetchSpeedData called");
+        
+        try {
+            const response = await fetch('/speedtest');
+            if (response.ok) {
+                setLastUpdated(new Date());
+                const data = await response.json();
+                setSpeedData(data[0]);
+                setSpeedRecords(data[1]);
+                setErrorMessage('');
+            } else {
+                setErrorMessage('An error occurred while fetching speed data.');
+            }
+        } catch (error) {
+            setErrorMessage('An error occurred while fetching speed data.');
+        }
     };
-
+    
     useEffect(() => {
-        fetchSpeedData();
-
-        const interval = setInterval(() => {
-            fetchSpeedData();
-        }, 60 * 60 * 1000); // Fetch data every 60 minutes
-
-        return () => {
-            clearInterval(interval);
+        const fetchAndSetSpeedData = async () => {
+            await fetchSpeedData();
+            const interval = setInterval(fetchSpeedData, 20 * 60 * 1000); // Fetch data every 20 minutes
+            return () => {
+                clearInterval(interval);
+            };
         };
+    
+        fetchAndSetSpeedData();
     }, []);
+     
+
 
     return (
         <Card bg={'secondary'} text={'light'} className={'mb-3'}>
             <Card.Body>
-                <Card.Title>Internet speed</Card.Title>
+                <Card.Title>INTERNET SPEED</Card.Title>
+
+                <hr className='border-gray mt-1' />
+
+                {errorMessage && <p>{errorMessage}</p>}
                 <Card.Text>
                     <Row>
                         <Col xs={6}>
@@ -36,7 +58,7 @@ const SpeedDisplay = () => {
                             {speedData ? (
                                 <h4>{(speedData.download / 1000000).toFixed(1)} <small>Mbps</small></h4>
                             ) : (
-                                <h4>Loading...</h4>
+                                <h4>...</h4>
                             )}
                         </Col>
 
@@ -45,7 +67,25 @@ const SpeedDisplay = () => {
                             {speedData ? (
                                 <h4>{(speedData.upload / 1000000).toFixed(1)} <small>Mbps</small></h4>
                             ) : (
-                                <h4>Loading...</h4>
+                                <h4>...</h4>
+                            )}
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xs={12} className="mt-3">
+                            <LineGraph data={speedRecords} />
+                        </Col>
+                    </Row>
+
+                    <hr className='border-gray mb-1' />
+
+                    <Row>
+                        <Col>
+                            {speedData ? (
+                                <small className='text-muted'>Updated <span> {lastUpdated.toLocaleTimeString()} </span></small>
+                            ) : (
+                                <small className='text-muted'>Updating now...</small>
                             )}
                         </Col>
                     </Row>
