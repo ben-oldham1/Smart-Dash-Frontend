1c5a1b56c207d243cc7b34a939dcfe35ecc536cc
Ben <ben.oldham2003@gmail.com>
2023-10-15 14:03:53 +0100

    Remove X axis

--- src/components/graphs/internetChart.js
+++ src/components/graphs/internetChart.js
@@ -3,15 +3,15 @@ import { LineChart, Line, XAxis, ResponsiveContainer } from 'recharts';
 
 const LineGraph = ({ data }) => {
   return (
-    <div style={{ width: '100%', height: '100px' }}>
+    <div style={{ width: '100%', height: '60px' }}>
 
       <ResponsiveContainer width="100%" height="100%">
         <LineChart
           width={300}
-          height={100}
+          height={60}
           data={data}
         >
-          <XAxis dataKey="time" />
+          
           <Line type="monotone" dataKey="download" stroke="#007bff" />
           <Line type="monotone" dataKey="upload" stroke="#dc3545" />
         </LineChart>
